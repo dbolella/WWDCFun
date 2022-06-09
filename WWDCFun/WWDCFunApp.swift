e764235549d3603ffd57904586759eacef125679
Danny Bolella <dbolella@gmail.com>
2022-06-09 11:45:41 -0400

    Initial Commit

---
+++ WWDCFun/WWDCFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCFunApp.swift
+//  WWDCFun
+//
+//  Created by Daniel Bolella on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WWDCFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
